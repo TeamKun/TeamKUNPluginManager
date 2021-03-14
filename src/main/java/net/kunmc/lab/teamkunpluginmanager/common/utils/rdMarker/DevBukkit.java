aa1305593afe158dc8114fa6b5786c9e9019adf4
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-03-14 22:04:40 +0900

    :bug: fix: NoClassDefFound

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/common/utils/rdMarker/DevBukkit.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/common/utils/rdMarker/DevBukkit.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import net.kunmc.lab.teamkunpluginmanager.utils.URLUtils;
+import net.kunmc.lab.teamkunpluginmanager.common.utils.URLUtils;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 
