a902955722b4beb98ec58460e6bde6cdfc557059
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-04-24 22:39:02 +0900

    :bug: fix: アップデートできない問題
    BREAKING CHANGE: configの書式変更

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/CommandUpdate.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/CommandUpdate.java
@@ -7,17 +7,25 @@ import net.kunmc.lab.teamkunpluginmanager.TeamKunPluginManager;
 import net.kunmc.lab.teamkunpluginmanager.plugin.Installer;
 import net.kunmc.lab.teamkunpluginmanager.plugin.KnownPluginEntry;
 import net.kunmc.lab.teamkunpluginmanager.plugin.KnownPlugins;
+import net.kunmc.lab.teamkunpluginmanager.utils.PluginUtil;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -44,13 +52,16 @@ public class CommandUpdate
 
         sender.sendMessage(ChatColor.LIGHT_PURPLE + "アップデートファイルのダウンロードを開始します...");
         CommandSender finalSender = sender;
+
         new BukkitRunnable()
         {
             @Override
             public void run()
             {
                 ArrayList<String> paths = new ArrayList<>();
-                TeamKunPluginManager.config.getStringList("config")
+                AtomicInteger num = new AtomicInteger(0);
+                new File(TeamKunPluginManager.DATABASE_PATH).mkdirs();
+                TeamKunPluginManager.config.getMapList("config")
                         .forEach(s -> {
                             String fileName;
 
@@ -61,33 +72,62 @@ public class CommandUpdate
                             }
                             while (new File(fileName).exists());
 
+                            String url = (String) s.get("url");
                             try
                             {
-                                URL urlObj = new URL(s);
+                                URL urlObj = new URL(url);
                                 HttpURLConnection connection = (HttpURLConnection) urlObj.openConnection();
                                 connection.setRequestMethod("GET");
-                                if (urlObj.getHost().equals("api.github.com"))
+                                if ((boolean) s.get("auth") && (urlObj.getHost().equals("api.github.com") ||
+                                        urlObj.getHost().equals("raw.githubusercontent.com")))
                                     connection.setRequestProperty("Authorization", "token " + TeamKunPluginManager.vault.getToken());
-                                connection.setRequestProperty("User-Agent", "TeamKUN Client");
+                                connection.setRequestProperty("User-Agent",
+                                        "Mozilla/8.10; Safari/Chrome/Opera/Edge/KungleBot-Peyang; Mobile-Desktop");
                                 connection.connect();
                                 if (connection.getResponseCode() != 200)
                                 {
-                                    finalSender.sendMessage(ChatColor.RED + "ファイルのダウンロード失敗しました。");
+                                    finalSender.sendMessage(ChatColor.RED + "無視：" + num.incrementAndGet() + " " + url);
+
                                     return;
                                 }
 
                                 File file = new File(fileName);
                                 paths.add(fileName);
-                                FileUtils.copyURLToFile(urlObj, file);
+                                if (!file.createNewFile())
+                                {
+                                    finalSender.sendMessage(ChatColor.GREEN + "ヒット：" + num.incrementAndGet() + " " + url);
+                                    return;
+                                }
+                                try(InputStream is = connection.getInputStream();
+                                    OutputStream os = new FileOutputStream(file))
+                                {
+                                    IOUtils.copy(is, os);
+                                }
+
+
+                                finalSender.sendMessage(ChatColor.GREEN + "取得：" + num.incrementAndGet() +
+                                        " " + url + " [" + PluginUtil.getFileSizeString(file.length()) + "]");
+                            }
+                            catch (MalformedURLException e)
+                            {
+                                finalSender.sendMessage(ChatColor.RED + "エラー：" + num.incrementAndGet() + " " + url);
+                                finalSender.sendMessage(ChatColor.RED + "  '" + url + "' はURLではありません。");
+                            }
+                            catch (UnknownHostException e)
+                            {
+                                finalSender.sendMessage(ChatColor.RED + "エラー：" + num.incrementAndGet() + " " + url);
+                                finalSender.sendMessage(ChatColor.RED + "  '" + url + "' を解決できませんでした。");
                             }
                             catch (Exception e)
                             {
+                                finalSender.sendMessage(ChatColor.RED + "エラー：" + num.incrementAndGet() + " " + url);
+                                finalSender.sendMessage(ChatColor.RED + "  エラー " + e.getClass().getName() + " が発生しました。");
                                 e.printStackTrace();
                             }
                         });
 
                 finalSender.sendMessage(ChatColor.GREEN + "ファイルのダウンロードに成功しました。");
-                finalSender.sendMessage(ChatColor.LIGHT_PURPLE + "データベースに登録しています...");
+                finalSender.sendMessage(ChatColor.LIGHT_PURPLE + "リストを読み込んでいます...");
 
                 AtomicInteger atomicInteger = new AtomicInteger(0);
 
@@ -132,7 +172,7 @@ public class CommandUpdate
                 });
 
                 finalSender.sendMessage(ChatColor.GREEN + "項目数: " + atomicInteger.get());
-                finalSender.sendMessage(ChatColor.GREEN + "S: 既知プラグインデータセットのアップデートに成功しました。");
+                finalSender.sendMessage(ChatColor.GREEN + "S: プラグイン定義ファイルのアップデートに成功しました。");
             }
         }.runTaskAsynchronously(TeamKunPluginManager.plugin);
     }
