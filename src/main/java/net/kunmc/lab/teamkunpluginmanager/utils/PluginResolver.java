840ee16dd5b4595c1b9fd1092024231c58a17e46
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-05-07 03:13:23 +0900

    :sparkles: feat: @でバージョン指定

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/utils/PluginResolver.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/utils/PluginResolver.java
@@ -20,27 +20,32 @@ public class PluginResolver
      */
     public static String asUrl(String query)
     {
+        String[] q = StringUtils.split(query, "@");
+
+        String s = q[0];
+        String ver = q.length > 1 ? q[1]: null;
+
         if (UrlValidator.getInstance().isValid(query))
         {
             if (DevBukkit.isMatch(query))
                 return DevBukkit.toDownloadUrl(query);
             else if (Spigotmc.isMatch(query))
                 return Spigotmc.toDownloadUrl(query);
-            return GitHubURLBuilder.urlValidate(query);
+            return GitHubURLBuilder.urlValidate(s, ver);
         }
 
         if (KnownPlugins.isKnown(query))
             return PluginResolver.asUrl(KnownPlugins.getKnown(query).url);
-        else if (StringUtils.split("/", query).length == 2)
-            return GitHubURLBuilder.urlValidate("https://github.com/" + query);
+        else if (StringUtils.split(query, "/").length == 2)
+            return GitHubURLBuilder.urlValidate("https://github.com/" + s, ver);
 
         //configのorgを順番にfetch
 
         Object obj = TeamKunPluginManager.plugin.getConfig().get("gitHubName");
 
         if (obj instanceof String)
-            if (GitHubURLBuilder.isRepoExists(obj + "/" + query))
-                return GitHubURLBuilder.urlValidate("https://github.com/" + obj + "/" + query);
+            if (GitHubURLBuilder.isRepoExists(obj + "/" + s))
+                return GitHubURLBuilder.urlValidate("https://github.com/" + obj + "/" + s, ver);
             else
                 return "ERROR " + query + "が見つかりませんでした。";
 
@@ -51,7 +56,7 @@ public class PluginResolver
         for (String str : TeamKunPluginManager.config.getStringList("gitHubName"))
         {
             if (GitHubURLBuilder.isRepoExists(str + "/" + query))
-                return GitHubURLBuilder.urlValidate("https://github.com/" + str + "/" + query);
+                return GitHubURLBuilder.urlValidate("https://github.com/" + str + "/" + s, ver);
         }
 
         return "ERROR " + query + "が見つかりませんでした。";
