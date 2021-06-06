44ebf0204db01a11c43ff77870005a662798f82a
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-06-07 01:40:29 +0900

    :sparkles: feat: PluginYamlParser

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/console/utils/PluginYamlParser.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/console/utils/PluginYamlParser.java
@@ -1,19 +1,23 @@
 package net.kunmc.lab.teamkunpluginmanager.console.utils;
 
 import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.internal.LinkedTreeMap;
 import net.kunmc.lab.teamkunpluginmanager.common.utils.ClassUtils;
+import org.apache.commons.lang3.SerializationUtils;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class PluginYamlParser
+public class PluginYamlParser implements Serializable
 {
     public String name;
     public String version;
@@ -31,6 +35,9 @@ public class PluginYamlParser
     public String[] loadbefore;
     public HashMap<String, Command> commands;
     public HashMap<String, Permission> permissions;
+    @SerializedName("default-permission")
+    private PrePermission defaultPrePermission;
+    public Permission defaultPermission;
 
     public static PluginYamlParser fromJar(File file) throws IOException
     {
@@ -54,12 +61,6 @@ public class PluginYamlParser
         }
     }
 
-    @SuppressWarnings("unchecked")
-    public static <T> T castAs(Object main)
-    {
-        return (T) main;
-    }
-
     public PluginYamlParser parse(HashMap<String, Object> kv)
     {
 
@@ -78,26 +79,117 @@ public class PluginYamlParser
             }
         }
 
-        return pluginYamlParser;
+        pluginYamlParser.defaultPermission = flatLine(pluginYamlParser.defaultPrePermission);
+
+        return flatLine(pluginYamlParser);
     }
 
-    enum Load
+    private PluginYamlParser flatLine(PluginYamlParser pluginYamlParser)
+    {
+        PluginYamlParser newParser = SerializationUtils.clone(pluginYamlParser);
+
+        if (pluginYamlParser.permissions == null)
+            return pluginYamlParser;
+
+        newParser.permissions = new HashMap<>();
+
+        pluginYamlParser.permissions.forEach((s, permission) -> {
+            HashMap<String, Object> children = permission.children;
+            if (children == null)
+            {
+                newParser.permissions.put(s, permission);
+                return;
+            }
+
+            permission.children = flatLine(children);
+
+            newParser.permissions.put(s, permission);
+        });
+
+        return newParser;
+    }
+
+    public HashMap<String, Object> flatLine(HashMap<String, Object> children)
+    {
+        if (children == null)
+            return null;
+
+        HashMap<String, Object> newChildren = new HashMap<>(children);
+
+        children.forEach((s1, o) -> {
+            if (o instanceof LinkedTreeMap)
+            {
+                PrePermission perm = new Gson().fromJson(new Gson().toJson(o), PrePermission.class);
+                perm.children = flatLine(perm.children);
+
+                newChildren.put(s1, flatLine(perm));
+            }
+            else
+                newChildren.put(s1, o);
+        });
+
+        return newChildren;
+    }
+
+    public Permission flatLine(PrePermission permission)
+    {
+        if (permission == null)
+            return null;
+
+        Permission perm = new Permission();
+
+        perm.description = permission.description;
+        perm.children = permission.children;
+
+        if (defaultPermission == null)
+            return perm;
+
+        if (permission.defaultPermission.equalsIgnoreCase("op"))
+            perm.defaultPermission = DefaultPermission.OP;
+        else if (permission.defaultPermission.equalsIgnoreCase("not op") ||
+                permission.defaultPermission.equalsIgnoreCase("not_op"))
+            perm.defaultPermission = DefaultPermission.NOTOP;
+        else if (permission.defaultPermission.equalsIgnoreCase("true"))
+            perm.defaultPermission = DefaultPermission.TRUE;
+        else if (permission.defaultPermission.equalsIgnoreCase("false"))
+            perm.defaultPermission = DefaultPermission.FALSE;
+        return perm;
+    }
+
+    public enum Load
     {
         STARTUP,
         POSTWORLD
     }
 
-    public static class Command
+    public static class Command implements Serializable
     {
         public String description;
         public String[] aliases;
+        public String permission;
+        @SerializedName("permission-message")
+        public String permissionMessage;
+        public String usage;
     }
 
-    public static class Permission
+    public static class Permission implements Serializable
     {
         public String description;
-
+        public DefaultPermission defaultPermission;
+        public HashMap<String, Object> children;
     }
 
+    public static class PrePermission extends Permission
+    {
+        @SerializedName("default")
+        public String defaultPermission;
+    }
 
+    public enum DefaultPermission
+    {
+        OP,
+        NOTOP,
+        FALSE,
+        TRUE
+    }
 }
