dc80b0efb130a8ebdd8189e6083471f29b343a47
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-03-24 00:13:56 +0900

    fix: AutoRemove Nullpo

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/CommandAutoRemove.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/CommandAutoRemove.java
@@ -10,22 +10,20 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CommandAutoRemove
 {
     public static ArrayList<String> onCommand(CommandSender sender, String[] args)
     {
-
-        if (sender != null && !sender.hasPermission("kpm.autoremove"))
+        if (!sender.hasPermission("kpm.autoremove"))
         {
             sender.sendMessage(ChatColor.RED + "E: 権限がありません！");
             return null;
         }
 
         ArrayList<String> rem = new ArrayList<>();
-        if (sender == null)
-            sender = Installer.dummySender();
 
         if (!TeamKunPluginManager.session.lock())
         {
@@ -35,6 +33,7 @@ public class CommandAutoRemove
 
         AtomicInteger removed = new AtomicInteger();
         sender.sendMessage(ChatColor.LIGHT_PURPLE + "依存関係ツリーを読み込み中...");
+
         ArrayList<String> removables = DependencyTree.unusedPlugins();
         if (removables.size() == 0)
         {
@@ -45,29 +44,37 @@ public class CommandAutoRemove
             return rem;
         }
 
+        while (removables.size() > 0)
+        { // のremoveUnusedPluginsで削除したプラグインによりいらない物ができた場合のためのループ
+            rem.addAll(removeUnusedPlugins(removables));
+            removables = DependencyTree.unusedPlugins();
+        }
+
+        rem.forEach(s -> {
+            sender.sendMessage(Messages.getModifyMessage(Messages.ModifyType.REMOVE, s));
+            removed.getAndIncrement();
+        });
+        sender.sendMessage(Messages.getStatusMessage(0, removed.get(), 0));
+
+        TeamKunPluginManager.session.unlock();
+        return rem;
+    }
+
+    private static List<String> removeUnusedPlugins(ArrayList<String> removables)
+    {
+        ArrayList<String> removed = new ArrayList<>();
+
         for (String removable : removables)
         {
             Plugin plugin = Bukkit.getPluginManager().getPlugin(removable);
             if (plugin != null)
             {
                 Installer.unInstall(null, removable, true);
-                rem.add(removable);
+                removed.add(removable);
             }
             DependencyTree.purge(removable);
         }
 
-        rem.addAll(onCommand(null, null));
-
-        if (!sender.equals(Installer.dummySender()))
-        {
-            CommandSender finalSender = sender;
-            rem.forEach(s -> {
-                finalSender.sendMessage(Messages.getModifyMessage(Messages.ModifyType.REMOVE, s));
-                removed.getAndIncrement();
-            });
-            sender.sendMessage(Messages.getStatusMessage(0, removed.get(), 0));
-        }
-        TeamKunPluginManager.session.unlock();
-        return rem;
+        return removed;
     }
 }
