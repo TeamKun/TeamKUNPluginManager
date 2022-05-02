b31d0acfcd8f580ad872349d39c04d03e3df00f0
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-05-02 14:57:43 +0900

    revert: wip: installer: Remove predicate

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/plugin/installer/phase/phases/predicate/PredicateErrorCause.java
+++
@@ -1,9 +0,0 @@
-package net.kunmc.lab.teamkunpluginmanager.plugin.installer.phase.phases.predicate;
-
-import net.kunmc.lab.teamkunpluginmanager.plugin.installer.phase.PhaseEnum;
-
-public enum PredicateErrorCause implements PhaseEnum
-{
-    CHECK_FAILED,
-    EXCEPTION_OCCURRED
-}
--- src/main/java/net/kunmc/lab/teamkunpluginmanager/plugin/installer/phase/phases/predicate/PredicateState.java
+++
@@ -1,9 +0,0 @@
-package net.kunmc.lab.teamkunpluginmanager.plugin.installer.phase.phases.predicate;
-
-import net.kunmc.lab.teamkunpluginmanager.plugin.installer.phase.PhaseEnum;
-
-public enum PredicateState implements PhaseEnum
-{
-    INITIALIZED,
-    CHECK_RUN
-}
