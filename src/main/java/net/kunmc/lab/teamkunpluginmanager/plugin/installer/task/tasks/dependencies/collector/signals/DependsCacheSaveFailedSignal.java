b7b74158dab3bd78bfe568db9c4afd0bbe14234a
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-09-04 07:29:14 +0900

    refactor: Remove unused signal: DependsCacheSaveFailedSignal

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/plugin/installer/task/tasks/dependencies/collector/signals/DependsCacheSaveFailedSignal.java
+++
@@ -1,7 +0,0 @@
-package net.kunmc.lab.teamkunpluginmanager.plugin.installer.task.tasks.dependencies.collector.signals;
-
-import net.kunmc.lab.teamkunpluginmanager.plugin.installer.InstallerSignal;
-
-public class DependsCacheSaveFailedSignal implements InstallerSignal
-{
-}
