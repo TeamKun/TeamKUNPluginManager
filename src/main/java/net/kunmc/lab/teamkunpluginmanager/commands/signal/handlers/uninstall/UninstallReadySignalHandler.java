cd838be2469b9dd6b5161353ecb4b776308e95c4
Peyang <peyang@peya.tokyo>
2022-09-12 22:34:49 +0900

    refactor: Extract UninstallReadySignal handler from UninstallerSignalHandler

---
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/signal/handlers/uninstall/UninstallReadySignalHandler.java
@@ -0,0 +1,55 @@
+package net.kunmc.lab.teamkunpluginmanager.commands.signal.handlers.uninstall;
+
+import net.kunmc.lab.peyangpaperutils.lib.terminal.QuestionAttribute;
+import net.kunmc.lab.peyangpaperutils.lib.terminal.QuestionResult;
+import net.kunmc.lab.peyangpaperutils.lib.terminal.Terminal;
+import net.kunmc.lab.teamkunpluginmanager.plugin.installer.impls.uninstall.signals.UninstallReadySignal;
+import net.kunmc.lab.teamkunpluginmanager.plugin.signal.SignalHandler;
+import net.kunmc.lab.teamkunpluginmanager.utils.Utils;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UninstallReadySignalHandler
+{
+    private final Terminal terminal;
+
+    public UninstallReadySignalHandler(Terminal terminal)
+    {
+        this.terminal = terminal;
+    }
+
+    private void printUninstallInfo(List<Plugin> uninstallTargets)
+    {
+        terminal.writeLine(ChatColor.GREEN + "以下のパッケージは「" + ChatColor.RED + "削除" + ChatColor.GREEN + "」されます。");
+        terminal.writeLine("  " + uninstallTargets.stream()
+                .map(Plugin::getName)
+                .sorted()
+                .collect(Collectors.joining(" ")));
+        Utils.printInstallStatistics(terminal, 0, uninstallTargets.size(), 0, 0);
+    }
+
+    private boolean pollContinue()
+    {
+        try
+        {
+            QuestionResult result = terminal.getInput().showYNQuestion("続行しますか?").waitAndGetResult();
+            return result.test(QuestionAttribute.YES);
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+            terminal.error("不明なエラーが発生しました: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @SignalHandler
+    public void onPluginsEnumerated(UninstallReadySignal signal)
+    {
+        printUninstallInfo(signal.getPlugins());
+        signal.setContinueUninstall(pollContinue());
+    }
+}
--- src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/signal/handlers/uninstall/UninstallerSignalHandler.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/signal/handlers/uninstall/UninstallerSignalHandler.java
@@ -1,9 +1,6 @@
 package net.kunmc.lab.teamkunpluginmanager.commands.signal.handlers.uninstall;
 
-import net.kunmc.lab.peyangpaperutils.lib.terminal.QuestionAttribute;
-import net.kunmc.lab.peyangpaperutils.lib.terminal.QuestionResult;
 import net.kunmc.lab.peyangpaperutils.lib.terminal.Terminal;
-import net.kunmc.lab.teamkunpluginmanager.plugin.installer.impls.uninstall.signals.UninstallReadySignal;
 import net.kunmc.lab.teamkunpluginmanager.plugin.installer.task.tasks.uninstall.signals.PluginDisablingSignal;
 import net.kunmc.lab.teamkunpluginmanager.plugin.installer.task.tasks.uninstall.signals.PluginRegisteredRecipeSignal;
 import net.kunmc.lab.teamkunpluginmanager.plugin.installer.task.tasks.uninstall.signals.PluginUninstallErrorSignal;
@@ -11,12 +8,7 @@ import net.kunmc.lab.teamkunpluginmanager.plugin.installer.task.tasks.uninstall.
 import net.kunmc.lab.teamkunpluginmanager.plugin.installer.task.tasks.uninstall.signals.PluginUnloadingSignal;
 import net.kunmc.lab.teamkunpluginmanager.plugin.signal.SignalHandler;
 import net.kunmc.lab.teamkunpluginmanager.utils.PluginUtil;
-import net.kunmc.lab.teamkunpluginmanager.utils.Utils;
 import org.bukkit.ChatColor;
-import org.bukkit.plugin.Plugin;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class UninstallerSignalHandler
 {
@@ -38,38 +30,6 @@ public class UninstallerSignalHandler
             default:
                 return "不明なエラーが発生しました。";
         }
-    }
-
-    private void printUninstallInfo(List<Plugin> uninstallTargets)
-    {
-        terminal.writeLine(ChatColor.GREEN + "以下のパッケージは「" + ChatColor.RED + "削除" + ChatColor.GREEN + "」されます。");
-        terminal.writeLine("  " + uninstallTargets.stream()
-                .map(Plugin::getName)
-                .sorted()
-                .collect(Collectors.joining(" ")));
-        Utils.printInstallStatistics(terminal, 0, uninstallTargets.size(), 0, 0);
-    }
-
-    private boolean pollContinue()
-    {
-        try
-        {
-            QuestionResult result = terminal.getInput().showYNQuestion("続行しますか?").waitAndGetResult();
-            return result.test(QuestionAttribute.YES);
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-            terminal.error("不明なエラーが発生しました: " + e.getMessage());
-            return false;
-        }
-    }
-
-    @SignalHandler
-    public void onPluginsEnumerated(UninstallReadySignal signal)
-    {
-        printUninstallInfo(signal.getPlugins());
-        signal.setContinueUninstall(pollContinue());
     }
 
     @SignalHandler
