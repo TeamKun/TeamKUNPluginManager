4f09be6b6f54dccbaf63b7b574768300b33fcb65
Peyang <peyang@peya.tokyo>
2022-10-02 13:47:17 +0900

    feat: Purge command

--- src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/debug/CommandDepTreeDebug.java
+++ src/main/java/net/kunmc/lab/teamkunpluginmanager/commands/debug/CommandDepTreeDebug.java
@@ -3,6 +3,7 @@ package net.kunmc.lab.teamkunpluginmanager.commands.debug;
 import net.kunmc.lab.peyangpaperutils.lib.command.CommandBase;
 import net.kunmc.lab.peyangpaperutils.lib.command.SubCommandWith;
 import net.kunmc.lab.teamkunpluginmanager.commands.debug.deptree.CommandMarkAsDependency;
+import net.kunmc.lab.teamkunpluginmanager.commands.debug.deptree.CommandPurge;
 import net.kunmc.lab.teamkunpluginmanager.commands.debug.deptree.CommandRelation;
 import net.kunmc.lab.teamkunpluginmanager.plugin.meta.PluginMetaManager;
 import net.kyori.adventure.text.TextComponent;
@@ -23,6 +24,7 @@ public class CommandDepTreeDebug extends SubCommandWith
 
         COMMANDS.put("markAsDependency", new CommandMarkAsDependency(manager.getProvider()));
         COMMANDS.put("relation", new CommandRelation(manager.getProvider()));
+        COMMANDS.put("purge", new CommandPurge(manager.getProvider()));
     }
 
     @Override
