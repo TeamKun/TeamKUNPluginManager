463f95b92ec64b7e3c2c9fd36373dd936454a7cd
Peyang <peyang@peya.tokyo>
2022-11-18 01:55:25 +0900

    feat(daemon): Pair mapping in KPMCollectors

--- KPMDaemon/src/main/java/net/kunmc/lab/kpm/utils/KPMCollectors.java
+++ KPMDaemon/src/main/java/net/kunmc/lab/kpm/utils/KPMCollectors.java
@@ -1,8 +1,13 @@
 package net.kunmc.lab.kpm.utils;
 
+import lombok.AllArgsConstructor;
+import net.kunmc.lab.peyangpaperutils.lib.utils.Pair;
+
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
@@ -15,6 +20,16 @@ public class KPMCollectors
     public static <T> ReversedCollector<T> toReversedList()
     {
         return new ReversedCollector<>();
+    }
+
+    public static <L, R, M extends Map<L, R>> PairMapper<L, R, M> toPairMap(Supplier<M> mapSupplier)
+    {
+        return new PairMapper<>(mapSupplier);
+    }
+
+    public static <L, R> PairMapper<L, R, HashMap<L, R>> toPairHashMap()
+    {
+        return new PairMapper<>(HashMap::new);
     }
 
     private static class ReversedCollector<T> implements Collector<T, List<T>, List<T>>
@@ -54,4 +69,44 @@ public class KPMCollectors
         }
     }
 
+    // pair to hashmap
+    @AllArgsConstructor
+    private static class PairMapper<L, R, M extends Map<L, R>> implements Collector<Pair<L, R>, M, M>
+    {
+        private final Supplier<M> supplier;
+
+        @Override
+        public Supplier<M> supplier()
+        {
+            return this.supplier;
+        }
+
+        @Override
+        public BiConsumer<M, Pair<L, R>> accumulator()
+        {
+            return (map, pair) -> map.put(pair.getLeft(), pair.getRight());
+        }
+
+        @Override
+        public BinaryOperator<M> combiner()
+        {
+            return (map1, map2) ->
+            {
+                map1.putAll(map2);
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<M, M> finisher()
+        {
+            return map -> map;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics()
+        {
+            return EnumSet.of(Characteristics.CONCURRENT);
+        }
+    }
 }
