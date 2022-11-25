d30a70bf91cf851ea9a6240b998c0916266b1825
Peyang <peyang@peya.tokyo>
2022-11-26 03:54:22 +0900

    fix(daemon): Upgrade cancel doesn't work

--- KPMDaemon/src/main/java/net/kunmc/lab/kpm/installer/impls/upgrade/PluginUpgrader.java
+++ KPMDaemon/src/main/java/net/kunmc/lab/kpm/installer/impls/upgrade/PluginUpgrader.java
@@ -266,6 +266,9 @@ public class PluginUpgrader extends AbstractInstaller<UpgradeArgument, UpgradeEr
 
                 ResolveResult result = this.resolveOne(plugin, query);
 
+                if (result == null)
+                    return null; // Cancelled
+
                 resolvedQueries.put(plugin, (SuccessResult) result);
             }
         }
