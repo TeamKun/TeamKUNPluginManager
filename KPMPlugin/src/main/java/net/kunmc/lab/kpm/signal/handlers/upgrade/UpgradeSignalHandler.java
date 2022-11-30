b7341a5edaa9ae18619ef82c44c8852448abe76f
Peyang <peyang@peya.tokyo>
2022-12-01 04:29:16 +0900

    fix(daemon): Remove plugin resolving ask

--- KPMPlugin/src/main/java/net/kunmc/lab/kpm/signal/handlers/upgrade/UpgradeSignalHandler.java
+++ KPMPlugin/src/main/java/net/kunmc/lab/kpm/signal/handlers/upgrade/UpgradeSignalHandler.java
@@ -17,31 +17,13 @@ import java.util.stream.Collectors;
 public class UpgradeSignalHandler
 {
     // ignore UpdateQueryRetrievedSignal
+    // ignore PluginResolveFailedSignal
 
     private final Terminal terminal;
-    private final int targetPluginCount;
-    private int currentPluginCount = 0;
-    private int currentPluginResolvedCount = 0;
 
-    public UpgradeSignalHandler(Terminal terminal, int targetPlugins)
+    public UpgradeSignalHandler(Terminal terminal)
     {
         this.terminal = terminal;
-        this.targetPluginCount = targetPlugins;
-    }
-
-    private boolean askCancelOrContinue(int currentCount)
-    {
-
-        if (currentCount == this.targetPluginCount)  // allow -1
-        {
-            this.terminal.warn("プラグインのアップグレードを中止します。");
-            return false;
-        }
-        else
-        {
-            this.terminal.info("この警告を無視してアップデートを続行できます。");
-            return SignalHandlingUtils.askContinue(this.terminal);
-        }
     }
 
     @SignalHandler
@@ -51,16 +33,6 @@ public class UpgradeSignalHandler
             return;  // implemented in #onResolveFailed(ResolveFailedSignal)
 
         this.terminal.warn("プラグイン " + signal.getSpecifiedPluginName() + " が見つかりませんでした。");
-        signal.setContinueUpgrade(this.askCancelOrContinue(++this.currentPluginCount));
-    }
-
-    @SignalHandler
-    public void onResolveFailed(ResolveFailedSignal signal)
-    {
-        this.terminal.warn("プラグイン " + signal.getPlugin().getName() +
-                " の解決は " +
-                signal.getErrorCause().name() + " で失敗しました。");
-        signal.setContinueUpgrade(this.askCancelOrContinue(++this.currentPluginResolvedCount));
     }
 
     @SignalHandler
@@ -101,7 +73,5 @@ public class UpgradeSignalHandler
         this.terminal.warn("プラグイン(不明) のアップグレードは " +
                 result.getTaskStatus() + " で " + result.getReason() +
                 "により失敗しました。");
-
-        signal.setContinueUpgrade(this.askCancelOrContinue(++this.currentPluginCount));
     }
 }
