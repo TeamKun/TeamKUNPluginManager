99e54fcb1d9bc39f4b978afcc2e0134950b42d2e
Peyang <peyang@peya.tokyo>
2022-12-26 22:18:37 +0900

    refactor: KPMUpdater => KPMUpgrader

---
+++ KPMUpgrader/src/main/java/net/kunmc/lab/plugin/kpmupgrader/KPMUpgrader.java
@@ -0,0 +1,52 @@
+package net.kunmc.lab.plugin.kpmupgrader;
+
+import net.kunmc.lab.kpm.KPMDaemon;
+import net.kunmc.lab.kpm.KPMEnvironment;
+import net.kunmc.lab.kpm.utils.versioning.Version;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.nio.file.Path;
+import java.util.Collections;
+
+public final class KPMUpgrader extends JavaPlugin
+{
+    private static final String KPM_NAME = "TeamKunPluginManager";
+
+    private KPMDaemon kpmDaemon;
+    private Version currentKPMVersion;
+
+    private void checkKPMEnabled()
+    {
+        if (this.getServer().getPluginManager().getPlugin(KPM_NAME) == null)
+        {
+            this.getLogger().severe("KPM がこのサーバーにインストールされていません。");
+            this.getLogger().info("KPMUpgrader6 は、 KPM による自動インストールでのみ使用できます。");
+            this.getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    @Override
+    public void onEnable()
+    {
+        this.checkKPMEnabled();
+
+        Plugin kpm = this.getServer().getPluginManager().getPlugin(KPM_NAME);
+        assert kpm != null;
+
+        Path dataDir = this.getDataFolder().toPath();
+        Path kpmDataFolder = kpm.getDataFolder().toPath();
+
+        this.kpmDaemon = new KPMDaemon(
+                KPMEnvironment.builder(this)
+                        .tokenPath(kpmDataFolder.resolve("token.dat"))
+                        .tokenKeyPath(kpmDataFolder.resolve("token_key.dat"))
+                        .metadataDBPath(dataDir.resolve("plugins.db"))
+                        .aliasesDBPath(dataDir.resolve("aliases.db"))
+                        .organizations(Collections.emptyList())
+                        .sources(Collections.emptyMap())
+                        .build()
+        );
+
+    }
+}
